9d03e508fcf14d38603ea58a86b8b44bf47781d3
AndreyEvtukh <andrey.evtukh@gmail.com>
2025-03-12 19:01:59 +0100

    add

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
   res.json("Hello World!");
+  res.send('GET home page');
   console.error(555)
 });
 
