34ea8f78b3a7cb4e478e09120a13bd0806e2957f
AndreyEvtukh <andrey.evtukh@gmail.com>
2025-03-12 20:09:45 +0100

    add 2

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/api", (req, res) => res.send("Express on Vercel 2"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3001, () => console.log("Server ready on port 3002."));
 
 module.exports = app;
