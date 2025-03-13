96c95d51d9eeef7f3e80003f27ac58b84021b2f4
AndreyEvtukh <andrey.evtukh@gmail.com>
2025-03-13 20:06:51 +0100

    update

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/api", (req, res) => res.send("Express on Vercel 2"));
-
-app.listen(3001, () => console.log("Server ready on port 3002."));
-
-module.exports = app;
