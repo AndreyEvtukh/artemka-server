a954797dce1c1c0735dfb13a6017a58829087860
AndreyEvtukh <andrey.evtukh@gmail.com>
2025-03-17 10:25:16 +0100

    фффф

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 // app.use(
 //     cors({origin: ['http://localhost:4201', 'http://localhost:3001', 'https://artemka-server.vercel.app']})
 // );
