d874e366f5af83e37dab2baa004b2162d0aaf546
AndreyEvtukh <andrey.evtukh@gmail.com>
2025-03-19 12:48:57 +0100

    56

--- api/index.ts
+++ api/index.ts
@@ -37,12 +37,47 @@ app.use("/api/send", (req, res, next) => {
 
 app.post('/api/send', async (req: Request, res: Response) => {
     resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: [process.env.MAILTRAP_USER],
-        subject: 'Hello World',
-        html: '<strong>It works!</strong>'
-    });
-})
+                from: 'Acme <onboarding@resend.dev>',
+                to: [process.env.MAILTRAP_USER],
+                subject: 'Hello World',
+                html: '<strong>It works!</strong>'
+            });
+    console.error(121212)
+    return res.status(200).json({"ok": true});
+
+    const {name, subject, email, message} = req.body;
+
+    // if (!name || !subject || !email || !message) {
+    //     return res.status(400).json({error: 'Missing required fields'});
+    // }
+
+    try {
+        const emailHtml = replacePlaceholders(templateContent, {
+            name,
+            subject,
+            email,
+            message,
+        });
+
+        const emailData = {
+            from: process.env.MAILTRAP_FROM,
+            to: process.env.MAILTRAP_USER,
+            subject: `New message from ${name} with subject ${subject}`,
+            html: emailHtml
+        };
+        // const {error} = await resend.emails.send(emailData);
+
+        // if (error) {
+        //     return res.status(400).json({error});
+        // }
+
+        res.status(200).json({ok: true});
+    } catch (error) {
+        res.status(500).json({ok: false, error: error});
+    }
+
+    res.end();
+});
 
 // app.post('/api/send', async (req: Request, res: Response) => {
 //     resend.emails.send({
