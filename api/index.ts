effbb9f9cb1e7119b858b4a3edc9fa13a6ff9c3b
AndreyEvtukh <andrey.evtukh@gmail.com>
2025-03-19 09:56:47 +0100

    56

--- api/index.ts
+++ api/index.ts
@@ -24,19 +24,19 @@ app.use(bodyParser.json())
 //     credentials: true,
 // }));
 
-app.use("/api/send", (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', 'https://artemka-dev.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods',
-        'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-    );
-    res.setHeader('Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    );
-    next();
-});
+// app.use("/api/send", (req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     res.setHeader('Access-Control-Allow-Origin', 'https://artemka-dev.vercel.app');
+//     res.setHeader('Access-Control-Allow-Methods',
+//         'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+//     );
+//     res.setHeader('Access-Control-Allow-Headers',
+//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+//     );
+//     next();
+// });
 
-app.post('/api/send', async (req: Request, res: Response) => {
+app.get('/api/send', async (req: Request, res: Response) => {
     console.error(111)
 })
 
