f40e7c7d6e56a0bcc36f21079dc05aaa6988bb8f
AndreyEvtukh <andrey.evtukh@gmail.com>
2025-03-20 10:52:55 +0100

    56

--- api/index.ts
+++ api/index.ts
@@ -9,15 +9,42 @@ import __app from "../__app";
 
 const replacePlaceholders = require('../email/replacePlaceholders');
 const app: Application = express();
+app.use(cors())
+
 require('dotenv').config();
-
 const templatePath = 'email/email-template.html';
+
+
 const templateContent = fs.readFileSync(templatePath, 'utf-8');
-
-
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-app.use(cors())
+
+app.options('/api/send', cors()) // enable pre-flight request for DELETE request
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
+
+app.post("/api/send", (req: Request, res: Response) => {
+    console.error(req.headers.host)
+    return res.status(200).json({ok: true}).end();
+})
+
+//heroku config:add \ HEROKU_OAUTH_ID=625595d0-9b97-4d31-8225-5f325e048584 \ HEROKU_OAUTH_SECRET=171bec24-9c16-47bb-8101-3f0657878877
+app.listen(3000)
 
 // app.use(cors({
 //     origin: ['https://artemka-dev.vercel.app', 'https://artemka-server.vercel.app'],
@@ -36,61 +63,61 @@ app.use(cors())
 //     next();
 // });
 
-app.use((req, res, next) => {
-    console.error(5655)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-    );
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    );
+// app.use((req, res, next) => {
+//     console.error(5655)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Methods',
+//         'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+//     );
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+//     );
+//
+//     if (req.method === 'OPTIONS') {
+//         return res.status(200).end();
+//     }
+//     next();
+// });
 
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-    next();
-});
-
-app.post('/api/send', async (req: Request, res: Response) => {
-    // return res.status(200).json({"ok": true});
-
-    const {name, subject, email, message} = req.body;
-
-    // if (!name || !subject || !email || !message) {
-    //     return res.status(400).json({error: 'Missing required fields'});
-    // }
-
-    try {
-        const emailHtml = replacePlaceholders(templateContent, {
-            name,
-            subject,
-            email,
-            message,
-        });
-
-        const emailData = {
-            from: process.env.MAILTRAP_FROM,
-            to: process.env.MAILTRAP_USER,
-            subject: `New message from ${name} with subject ${subject}`,
-            html: emailHtml
-        };
-        const {error} = await resend.emails.send(emailData);
-
-        // if (error) {
-        //     return res.status(400).json({error});
-        // }
-        console.error(req.body)
-        res.status(200).json({ok: true});
-    } catch (error) {
-        res.status(500).json({ok: false, error: error});
-    }
-
-    res.end();
-});
+// app.post('/api/send', async (req: Request, res: Response) => {
+//     // return res.status(200).json({"ok": true});
+//
+//     const {name, subject, email, message} = req.body;
+//
+//     // if (!name || !subject || !email || !message) {
+//     //     return res.status(400).json({error: 'Missing required fields'});
+//     // }
+//
+//     try {
+//         const emailHtml = replacePlaceholders(templateContent, {
+//             name,
+//             subject,
+//             email,
+//             message,
+//         });
+//
+//         const emailData = {
+//             from: process.env.MAILTRAP_FROM,
+//             to: process.env.MAILTRAP_USER,
+//             subject: `New message from ${name} with subject ${subject}`,
+//             html: emailHtml
+//         };
+//         const {error} = await resend.emails.send(emailData);
+//
+//         // if (error) {
+//         //     return res.status(400).json({error});
+//         // }
+//         console.error(req.body)
+//         res.status(200).json({ok: true});
+//     } catch (error) {
+//         res.status(500).json({ok: false, error: error});
+//     }
+//
+//     res.end();
+// });
 
 // app.post('/api/send', async (req: Request, res: Response) => {
 //     resend.emails.send({
